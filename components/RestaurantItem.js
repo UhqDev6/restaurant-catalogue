13267d9a5d75795c6bd81ea3bcc0a58d2e2f8e66
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-01-11 12:48:29 +0800

    styling card using styled components

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -6,22 +6,23 @@ import Container from "./styled/Container";
 import Image from "./styled/Image";
 import Paragraph from "./styled/Paragraph";
 import WrapperParagraph from "./styled/WrapperParagraph";
+import Title from "./styled/Title";
 
 export default function RestaurantItem({ id, pictureId, name, description, isDetails}) {
   return (
     <Card>
       <Image src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`} alt={name} />
       <Container>
-        <h2>
+        <Title>
           <Link href={`/detail/${id}`}>{name}</Link>
-        </h2>
-        <div>
+        </Title>
+        <WrapperParagraph>
           <Paragraph className={`${isDetails ? '' : ''}`}>
             {
               description || parse(description)
             }
           </Paragraph>
-        </div>
+        </WrapperParagraph>
         <br />
       </Container>
     </Card>
--- components/styled/WrapperParagraph.js
+++ components/styled/WrapperParagraph.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
 const WrapperParagraph = styled.div`
-  -webkit-line-clamp: 3;
+  ${'' /* -webkit-line-clamp: 3;
   line-clamp: 3;
   -webkit-box-orient: vertical;
-  display: -webkit-box;
+  display: -webkit-box; */}
+    display:block;
+    padding:0px;
+    width: 100%;
+    height: 150px;
+    overflow:scroll;
 `;
 
 export default WrapperParagraph;
